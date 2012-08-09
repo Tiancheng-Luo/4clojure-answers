e658283574919763aa8569152be8dc6f0e10dd5b
emanon001 <emanon001@gmail.com>
2012-08-09 20:00:36 +0900

    Added problem 24 answer.

---
+++ src/four_clojure_answers/problem_024.clj
@@ -0,0 +1,4 @@
+(ns four-clojure-answers.problem-024)
+
+(def answer-024
+  (partial apply +))
---
+++ test/four_clojure_answers/problem_024_test.clj
@@ -0,0 +1,12 @@
+(ns four-clojure-answers.problem-024-test
+  (:use four-clojure-answers.problem-024
+        clojure.test))
+
+(deftest problem-024-test
+  (are [coll expected]
+    (= (answer-024 coll) expected)
+    [1 2 3] 6
+    (list 0 -2 5 5) 8
+    #{4 2 1} 7
+    '(0 0 -1) -1
+    '(1 10 3) 14))
